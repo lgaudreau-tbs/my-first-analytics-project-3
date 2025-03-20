2af547b50a702438de195e6f16a5c2b5400b989e
lgaudreau-tbs <l.gaudreau@tbs-education.org>
2025-03-20 16:51:46 +0000

    Visualizing car data sets

--- app.py
+++ app.py
@@ -1,31 +1,25 @@
 import streamlit as st
+import pandas as pd
 
-st.title("My Streamlit Web App")
-st.header("Data Exploration Section")
+st.title("Import a CSV File and Visualize with Streamlit Charts ")
 
-# Contents in  a sidebar
-with st.sidebar:
-    st.header("This is a sidebar header")
-    user_input = st.text_input("Enter your name")
-    st.write("Your provided name is ", user_input)
-    option = st.selectbox("Choose an option", ["Choice A", "Choice B", "Choice C"])
+df = pd.read_csv("Datasets/trips_data_1000.csv")
+st.write(" Preview Uploaded data")
+st.dataframe(df.head())
 
-# Contents in the main window
-# Display user input
-st.write(f"Hello, {user_input}! You selected option {option}.")
+# Chart 1: Bar chart of customers by country
+st.subheader("Customers by City")
+country_counts = df['customer_city'].value_counts()
+st.write(country_counts)
+st.bar_chart(country_counts)
 
-# Adding Contents in multiple columns
-col1, col2 = st.columns(2)
-with col1:
-    st.subheader("Column 1")
-    st.button("Click me!")
-    st.write("This is some text in column 1")
+# Chart 2: Line chart of Tripvcs over time
+st.subheader("Subscriptions Over Time")
+df['Trips Date'] = pd.to_datetime(df['pickup_time']).dt.date
+subscriptions_by_date = df.groupby('Trips Date').size().reset_index(name='Count')
+st.write(subscriptions_by_date)
+st.line_chart(subscriptions_by_date, x="Trips Date", y="Count")
 
-with col2:
-    st.subheader("Column 2")
-    st.line_chart({"data": [1, 5, 2, 6, 2, 1]})
 
-# Expandable section
-with st.expander("See explanation"):
-    st.write("This is an expandable section with additional information.")
+
 
